c98c2bb2fc3278442b41ed42fe9e68b762629c69
JaimeAven <jaime.aven.villa@gmail.com>
2026-08-31 22:00:21 -0500

    Add ProductDetails screen with initial implementation and styles

---
+++ src/screens/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const ProductDetails = () => {
+  return (
+    <View>
+      <Text>ProductDetails</Text>
+    </View>
+  )
+}
