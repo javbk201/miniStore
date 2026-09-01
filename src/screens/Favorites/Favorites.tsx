d53ef16c37b75875a72dc2887030c42fdafa9aa3
JaimeAven <jaime.aven.villa@gmail.com>
2026-08-31 22:00:01 -0500

    Add Favorites screen with initial implementation and styles

---
+++ src/screens/Favorites/Favorites.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const Favorites = () => {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  )
+}
